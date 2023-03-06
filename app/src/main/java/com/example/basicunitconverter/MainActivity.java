b2b3bdd5b3f94fb1825bde4d77baab41c64c9353
jyhe <jyhe.mark@hurtigruten.com>
2023-03-06 21:38:31 +0100

    Added all controls, still needs layout constraints

--- app/src/main/java/com/example/basicunitconverter/MainActivity.java
+++ app/src/main/java/com/example/basicunitconverter/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.basicunitconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindSpinners();
+    }
+
+    private void bindSpinners() {
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.unit_types, androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item);
+        arrayAdapter.setDropDownViewResource(androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item);
+
+        bindSpinner(R.id.sourceUnitSpinner, arrayAdapter);
+        bindSpinner(R.id.targetUnitSpinner, arrayAdapter);
+    }
+
+    private void bindSpinner(int spinnerId, ArrayAdapter<CharSequence> arrayAdapter){
+        Spinner spinner = findViewById(spinnerId);
+
+        if (spinner == null)
+            throw new NullPointerException("Spinner could not be bound");
+
+        spinner.setAdapter(arrayAdapter);
+    }
+
+    public void convertButtonOnClick(View view){
+        Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show();
     }
 }
