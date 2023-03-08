19d7414c6db9647045e61b5183c5cb4448a40ad8
jyhe <jyhe.mark@hurtigruten.com>
2023-03-08 17:59:20 +0100

    Updated conversions to US ton instead of metric

--- app/src/main/java/com/example/basicunitconverter/WeightConverter.java
+++ app/src/main/java/com/example/basicunitconverter/WeightConverter.java
@@ -10,22 +10,22 @@ public class WeightConverter extends Converter {
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.POUND, UnitTypes.Unit.OUNCE), amt -> {return amt * 16;});
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.POUND, UnitTypes.Unit.TON), amt -> {return amt / 2000;});
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.POUND, UnitTypes.Unit.GRAM), amt -> {return amt * 453.6;});
-        _conversionSets.put(new ConversionSet(UnitTypes.Unit.POUND, UnitTypes.Unit.KILOGRAM), amt -> {return amt / 2.205;});
+        _conversionSets.put(new ConversionSet(UnitTypes.Unit.POUND, UnitTypes.Unit.KILOGRAM), amt -> {return amt * 0.453592;});
 
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.OUNCE, UnitTypes.Unit.POUND), amt -> {return amt * 16;});
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.OUNCE, UnitTypes.Unit.TON), amt -> {return amt / 32000;});
-        _conversionSets.put(new ConversionSet(UnitTypes.Unit.OUNCE, UnitTypes.Unit.GRAM), amt -> {return amt * 28.35;});
+        _conversionSets.put(new ConversionSet(UnitTypes.Unit.OUNCE, UnitTypes.Unit.GRAM), amt -> {return amt * 28.3495;});
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.OUNCE, UnitTypes.Unit.KILOGRAM), amt -> {return amt / 35.274;});
 
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.TON, UnitTypes.Unit.OUNCE), amt -> {return amt * 35270;});
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.TON, UnitTypes.Unit.POUND), amt -> {return amt * 2205;});
-        _conversionSets.put(new ConversionSet(UnitTypes.Unit.TON, UnitTypes.Unit.GRAM), amt -> {return amt * 1000000;});
-        _conversionSets.put(new ConversionSet(UnitTypes.Unit.TON, UnitTypes.Unit.KILOGRAM), amt -> {return amt * 1000;});
+        _conversionSets.put(new ConversionSet(UnitTypes.Unit.TON, UnitTypes.Unit.GRAM), amt -> {return amt * 907185;});
+        _conversionSets.put(new ConversionSet(UnitTypes.Unit.TON, UnitTypes.Unit.KILOGRAM), amt -> {return amt * 907.185;});
 
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.GRAM, UnitTypes.Unit.OUNCE), amt -> {return amt * 28.35;});
-        _conversionSets.put(new ConversionSet(UnitTypes.Unit.GRAM, UnitTypes.Unit.TON), amt -> {return amt / 907200;});
+        _conversionSets.put(new ConversionSet(UnitTypes.Unit.GRAM, UnitTypes.Unit.TON), amt -> {return amt / 907185;});
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.GRAM, UnitTypes.Unit.POUND), amt -> {return amt / 453.6;});
-        _conversionSets.put(new ConversionSet(UnitTypes.Unit.GRAM, UnitTypes.Unit.KILOGRAM), amt -> {return amt / 1000;});
+        _conversionSets.put(new ConversionSet(UnitTypes.Unit.GRAM, UnitTypes.Unit.KILOGRAM), amt -> {return amt / 907.185;});
 
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.KILOGRAM, UnitTypes.Unit.OUNCE), amt -> {return amt * 35.274;});
         _conversionSets.put(new ConversionSet(UnitTypes.Unit.KILOGRAM, UnitTypes.Unit.TON), amt -> {return amt / 907.2;});
